4fca00cad84ce4bfc88b1f928ea247c7c98e563d
Marcos Pires <marcos.danix@gmail.com>
2018-03-08 00:38:15 +0000

    Successful Wall Generation in Play Mode
    It generates a small corridor in the center of the scene. I provided a test material with a test texture.

--- Assets/Scripts/WallCreator.cs
+++ Assets/Scripts/WallCreator.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class WallCreator : MonoBehaviour {
 
-    public Vector2 LowerLeft;
-    public Vector2 UpperRight;
+    public Vector2Int LowerLeft;
+    public Vector2Int UpperRight;
+    public Vector2 TexCoordOrigin;
+    public Vector2 TexCoordSize;
     public PerlinModulationSettings settings;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        int resX = settings.resolutionPerUnitX * (UpperRight.x - LowerLeft.x);
+        int resY = settings.resolutionPerUnitY * (UpperRight.y - LowerLeft.y);
+        MeshAttributes plane = new PlaneGenerator(LowerLeft, UpperRight, TexCoordOrigin, TexCoordSize, resX, resY).Generate();
+
+        Vector2 invResolution = new Vector2(1.0f / resX, 1.0f / resY);
+
+
+        Vector2 size = UpperRight - LowerLeft;        
+        Vector2 distPos = Vector2.Scale(size, invResolution);
+
+        Vector2 invTexSize = new Vector2(1.0f / TexCoordSize.x, 1.0f / TexCoordSize.y);
+        Vector2 texFullSize = Vector2.Scale(size, invTexSize);
+        Vector2 distTex = Vector2.Scale(texFullSize, invResolution);
+
+
+        PerlinModulator perlinModulator = new PerlinModulator()
+        {
+            frequency = settings.frequency,
+            amplitude = settings.amplitude,
+            octaves = settings.octaves,
+            world = transform.localToWorldMatrix,
+            seed = settings.seed,
+            seedX = settings.seedX,
+            seedY = settings.seedY,
+            uvDistPos = distPos,
+            uvDistTex = distTex,
+            uvDisplacement = new Vector2(settings.horizontalAmplitudeMultiplier, settings.verticalAmplitudeMultiplier),
+            vMin = LowerLeft.y,
+            vMax = UpperRight.y
+        };
+
+        Mesh mesh = perlinModulator.Modulate(plane);
+        GetComponent<MeshFilter>().mesh = mesh;
 	}
 	
 	// Update is called once per frame
